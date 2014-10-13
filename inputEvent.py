4906d57086dc28928dc2401b1a6a1a99fd97a19f
Oskar Wyke <oskar.wyke@gmail.com>
2014-10-13 19:24:04 +0000

    Some raspi scripts

---
+++ inputEvent.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+ledstatus = False
+
+def toggleLed(chan):
+  global ledstatus 
+  ledstatus = not ledstatus
+  GPIO.output(4, ledstatus)
+
+
+GPIO.add_event_detect(21, GPIO.RISING, callback=toggleLed, bouncetime=300)
+
+while True:
+   print(".")
+   time.sleep(1)
